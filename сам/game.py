ad3ce159c0dc4e421cff571d464e90ec2459fb67
Fedot-Danetot <shunko.fa@phystech.edu>
2024-11-18 13:03:06 +0300

    movement of models via move_ip(), correct creation of "enemys"

--- сам/game.py
+++ сам/game.py
@@ -1,128 +1,124 @@
-import pygame as pg 
-import random as rnd
+import pygame as pg
 import sys
+import random as rnd
+import math as m
+pg.init()
 
-
-pg.init() 
-
+screen = pg.display.set_mode((1024, 768))
 
 def draw_back():
-        '''отвечает за отрисовку окружения'''
-        color_back = (255, 228, 205)
-        color_border = (205, 178, 155)
-        border = pg.Rect(14, 11, 996, 746)
-        screen.fill(color_back)
-        pg.draw.rect(screen, color_border, border, 3) 
+    '''отвечает за отрисовку окружения''' 
+    color_back = (255, 228, 205)
+    color_border = (205, 178, 155)
+    border = pg.Rect(14, 11, 996, 746) # граница игрового поля
+    screen.fill(color_back)
+    pg.draw.rect(screen, color_border, border, 3)
+    
 
-
-class Tgg:
-
-    def __init__(self, x, y):
-        x = 502  
-        y = 374
-        self.x = x
-        self.y = y
-        
-
-    def draw(self, screen):
-        '''Рисует модельку gg по координатам x, y'''
-        gg = pg.Rect(self.x, self.y, 20, 20) 
-        pg.draw.rect(screen, color_gg, gg, 0)
-
+class Player:
+    def __init__(self):
+        '''Автоматически задает свойства модели игрока'''
+        self.color_player = (0, 0, 255)
+        self.player_model = pg.Rect(502, 374, 20, 20) # моделька игрока - пока квадрат
+        self.speed = 6 # скорость игрока
 
     def moving(self):
-        '''Описывает движение игрока (gg)'''
-        pressed = pg.key.get_pressed() 
-        if pressed[pg.K_w]:  
-            if self.y >= 17:    # ограничение для того, чтобы персонаж не выходил за рамки поля
-                self.y -= 5
-        if pressed[pg.K_s]:  
-            if self.y <= 731: 
-                self.y += 5 
-        if pressed[pg.K_a]:  
-            if self.x >= 18:     
-                self.x -= 5 
-        if pressed[pg.K_d]: 
-            if self.x <= 986:  
-                self.x += 5
-        return self.x, self.y
+        '''Отвечает за передвижение модели игрока'''
+        move_x, move_y = 0, 0
 
+        event = pg.key.get_pressed()
+        if event[pg.K_a] and self.player_model.center[0] > 20: # ограничение движения по границе игрового поля
+            move_x = -self.speed
+        if event[pg.K_d] and self.player_model.center[0] < 1004:
+            move_x = self.speed
+        if event[pg.K_s] and self.player_model.center[1] < 756:
+            move_y = self.speed
+        if event[pg.K_w] and self.player_model.center[1] > 21:
+            move_y = -self.speed
+        
+        if move_x != 0 and move_y != 0: # вычисление диагонального вектора при нескольких нажатых клавиш
+            length = m.sqrt(move_x**2 + move_y**2)
+            move_x = (move_x / length) * self.speed
+            move_y = (move_y / length) * self.speed
 
-class TEnemy:
+        self.player_model.move_ip(move_x, move_y)
+        pg.draw.rect(screen, self.color_player, self.player_model, 0)
 
-    def __init__(self):
+    def game_over(self):
+        '''Реализует проигрыш'''
+        # TODO
         pass
 
+
+class Enemy:
+    def __init__(self):
+        self.color_enemy = (255, 0, 0)
+        self.enemys = []
+        self.create_time = 2000
+        self.last_create_time = 0
+        self.speed = 3
+        self.player = Player()
 
     def spawn(self):
-        '''спавнит врагов раз в какое-то время'''
-        k = rnd.randrange(1, 5)
-        if k == 1:
-            i = rnd.randrange(1, 1004, 100)
-            enemy = pg.Rect(i, 1, 20, 20) 
-            pg.draw.rect(screen, color_enemy, enemy, 0)
-        elif k == 2:
-            i = rnd.randrange(1, 1004, 100)
-            enemy = pg.Rect(i, 748, 20, 20) 
-            pg.draw.rect(screen, color_enemy, enemy, 0)
-        elif k == 3:
-            i = rnd.randrange(1, 748, 100)
-            enemy = pg.Rect(1, i, 20, 20) 
-            pg.draw.rect(screen, color_enemy, enemy, 0)
-        elif k == 4:
-            i = rnd.randrange(1, 748, 100)
-            enemy = pg.Rect(1004, i, 20, 20) 
-            pg.draw.rect(screen, color_enemy, enemy, 0)
+        '''спавнит врагов раз в какое-то время''' 
+        current_time = pg.time.get_ticks() 
+        if current_time - self.last_create_time > self.create_time: 
+            k = rnd.randrange(1, 5) 
+            i = self._get_random_position(k) # определяем точку спавна с помощью следующей функции
+            enemy = pg.Rect(*i, 20, 20)  
+            self.enemys.append(enemy) 
+            self.last_create_time = current_time 
+
+    def _get_random_position(self, k):
+        '''Случайным образом определяет точку спавна противника'''
+        if k == 1: 
+            return rnd.randrange(1, 1004, 100), 1 
+        elif k == 2: 
+            return rnd.randrange(1, 1004, 100), 748 
+        elif k == 3: 
+            return 1, rnd.randrange(1, 748, 100) 
+        elif k == 4: 
+            return 1004, rnd.randrange(1, 748, 100)
+
+    def moving(self, i):
+        '''Определяет законы движения i-ого врага'''
+        direction = pg.Vector2(self.player.player_model.center) - pg.Vector2(self.enemys[i].center) # проблема в том, что вызывается начальный атрибут - противники бегут в точку спавна гг
+        if direction.length() > 0:
+            direction.normalize_ip()
+            self.enemys[i].move_ip(direction * self.speed)
+
+    def draw(self, i):
+        '''Отрисовывает i-ого врага'''
+        pg.draw.rect(screen, self.color_enemy, self.enemys[i])
 
 
-    def enemy_moving(x: int, y: int, e_x: int, e_y: int):
-        """Описывает движение врагов"""
-        # должны двигаться напрямую к gg
-        pass
+    def moving_draw_all(self):
+        '''реализует две предыдущих функции для всех врагов'''
+        for i in range(len(self.enemys)):
+            Enemy.moving(self, i)
+            Enemy.draw(self, i)
 
 
-    def enemy_attack():
-        '''По сути реализует проигрыш'''
-        pass
-
-
-
-class TBullet:
-    def spawn():
-        '''Описывает создание пули'''
-        pass
-
-    def moving():
-        '''Задает движение пули'''
-        pass
-
-    def kills():
-        '''Описание сценария "enemy is dead"'''
-        pass
-
- 
-color_gg = (0, 0, 255)
-color_enemy = (255, 0, 0)
-screen = pg.display.set_mode((1024, 768)) 
 clock = pg.time.Clock()
-enemys = pg.sprite.Group()
-
 
 def main():
-    '''Запускает основной цикл программы'''
     finish = False
-    Gg = Tgg(502, 374) # числа в скобках - координаты спавна гг
-    Enemy = TEnemy()
-    while not finish: 
-        for event in pg.event.get(): 
-            if event.type == pg.QUIT: 
+    draw_back()
+    player = Player()
+    enemys = Enemy()
+
+    while not finish:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 finish = True
-        
+
         draw_back()
-        Gg.moving()
-        Gg.draw(screen)
-        Enemy.spawn()
-        pg.display.flip() 
+        player.moving()
+        enemys.spawn()
+        enemys.moving_draw_all()
+
+        player.game_over()
+        pg.display.flip()
         clock.tick(30)
 
 main()
